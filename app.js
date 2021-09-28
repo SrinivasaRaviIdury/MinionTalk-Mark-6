655facdf96098790a4f779f4420607049eac7b27
SrinivasaRaviAhlad <raviaahlad@gmail.com>
2021-09-28 23:48:32 +0530

    code:minion translator completed

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 var buttonSelector = document.querySelector('#btn-translate');
-buttonSelector.addEventListener('click', function inputHandler() {
-  console.log('hello');
-});
+var inputSelector = document.querySelector('#inputText');
+var outputSelector = document.querySelector('#output');
+var url = 'https://api.funtranslations.com/translate/minion.json';
+
+function urlProcessor() {
+  var inputText = inputSelector.value;
+  return (inputString = url + '?' + 'text=' + inputText);
+}
+function inputHandler() {
+  var url = urlProcessor();
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputSelector.innerHTML = translatedText;
+    })
+    .catch((error) => console.log(error));
+}
+buttonSelector.addEventListener('click', inputHandler);
